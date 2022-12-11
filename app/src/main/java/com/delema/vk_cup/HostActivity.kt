ec99936e32af73030d0305dfcae9919cca5a534b
aNOOBis666 <mr.zooko@mail.ru>
2022-12-11 08:02:55 +0300

    androidX removed

--- app/src/main/java/com/delema/vk_cup/HostActivity.kt
+++ app/src/main/java/com/delema/vk_cup/HostActivity.kt
@@ -1,9 +1,8 @@
 package com.delema.vk_cup
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
 import com.delema.vk_cup.navigation.IFragmentsNavigation
 import com.delema.vk_cup.preferences_choosing_screen.PreferencesChoosingFragment
 
@@ -11,15 +10,14 @@ class HostActivity : AppCompatActivity(R.layout.ac_host), IFragmentsNavigation {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                openFragment(PreferencesChoosingFragment())
-            }
+            openFragment(PreferencesChoosingFragment())
         }
     }
 
     override fun openFragment(fragment: Fragment) {
-        supportFragmentManager.commit {
-            replace(R.id.fragment_container, fragment)
-        }
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
